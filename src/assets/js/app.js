acceb27222a5821ae5f408ceb7c7a521b10fc242
Jake Armstrong <Lainkai@users.noreply.github.com>
2020-02-13 23:24:26 -0500

    Floaty Space Kitty

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import 'what-input';
+import anime from 'animejs/lib/anime.es.js';
 
 // Foundation JS relies on a global variable. In ES6, all imports are hoisted
 // to the top of the file so if we used `import` to import Foundation,
@@ -16,4 +17,16 @@ require('particles.js')
 particlesJS.load('particles-js', '/assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+anime({
+  targets: '#h-top',
+  keyframes: [
+    {translateY: '-5vh'},
+    {translateY: '0vh'},
+  ],
+  duration:12000, 
+  loop: true,
+  easing: 'easeInOutSine',
+  delay: anime.stagger(1000)
+});
 $(document).foundation();
